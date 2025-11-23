8abbb30a144becf6bc52beaf572be7ab57a5abbd
Richer <81913556+Mishka-Web@users.noreply.github.com>
2025-11-23 10:09:32 +0500

    Новый дизайн

--- app/main.js
+++ app/main.js
@@ -3,19 +3,84 @@ const app = window.Telegram.WebApp;
 let cart = [];
 let total = 0;
 
-// new Swiper(".swiper", {
-// 	slidesPerView: 1,
-// 	spaceBetween: 10,
-// 	speed: 750,
-// 	loop: false,
-// });
+const sliders = document.querySelectorAll(".swiper");
+
+sliders.forEach((swiper) => {
+	new Swiper(swiper, {
+		slidesPerView: 1,
+		spaceBetween: 10,
+		speed: 750,
+	});
+});
+
+(function () {
+	const themeTokens = {
+		light: {
+			"--c-bg": "#ffffff",
+			"--c-bg-secondary": "#f6f6f7",
+			"--c-text": "#111111",
+			"--c-text-secondary": "#6b6b6c",
+			// "--c-accent": "#ff7795",
+			"--c-accent": "#FABEC2",
+			"--c-border": "#e5e5e5",
+			"--linear-gradient":
+				"linear-gradient(63deg, #FABEC2 0%, #FFDFE3 100%)",
+			"--transition": "all 0.3s ease-in-out",
+			"--c-card-image-color": "#fdfcf9",
+			"--c-card-image-bg": "url('./assets/img/bg-light.png?v=1')",
+			"--c-card-btn-order-bg": "var(--linear-gradient)",
+			"--c-card-btn-order-text": " #ffffff",
+			"--c-card-shadow": "0px 4.57px 54.85px 0 rgba(197, 197, 197, 0.25)",
+			"--c-card-bg": "#ffffff",
+			"--c-card-text": " #111111",
+
+			"--c-card-decor-1-url": "url('./assets/img/decor-1-light.png?v=1')",
+			"--c-card-decor-2-url": "url('./assets/img/decor-2-light.png?v=1')",
+		},
+		dark: {
+			"--c-bg": "#0d0d0f",
+			"--c-bg-secondary": "#161618",
+			"--c-text": "#f1f1f2",
+			"--c-text-secondary": "#a1a1a3",
+			"--c-accent": "#981D36",
+			"--c-border": "#2a2a2d",
+			"--linear-gradient":
+				"linear-gradient(63deg, #981D36 0%, #371317 100%)",
+			"--transition": "all 0.3s ease-in-out",
+			"--c-card-image-color": "#0d0d0f8c",
+			"--c-card-image-bg": "url('./assets/img/bg.png?v=1')",
+			"--c-card-btn-order-bg": "var(--linear-gradient)",
+			"--c-card-btn-order-text": " #ffffff",
+			"--c-card-shadow": "none",
+			"--c-card-bg": "#1F1F1F",
+			"--c-card-text": " #FFFFFF",
+
+			"--c-card-decor-1-url": "url('./assets/img/decor-1.png?v=1')",
+			"--c-card-decor-2-url": "url('./assets/img/decor-2.png?v=1')",
+		},
+	};
+
+	function applyTheme() {
+		const isDark = app.colorScheme === "dark";
+		const tokens = isDark ? themeTokens.light : themeTokens.light;
+
+		document.documentElement.classList.toggle("dark", isDark);
+
+		for (const key in tokens) {
+			document.documentElement.style.setProperty(key, tokens[key]);
+		}
+	}
+
+	if (app.onEvent) {
+		app.onEvent("themeChanged", applyTheme);
+	} else {
+		app.onThemeChanged = applyTheme;
+	}
+
+	applyTheme();
+})();
 
 app.ready();
-
-app.onEvent("themeChanged", () => {
-	const isDark = app.colorScheme === "dark";
-	document.body.classList.toggle("dark", isDark);
-});
 
 const checkoutBtn = document.getElementById("checkout");
 checkoutBtn.style.display = "none";
@@ -35,16 +100,26 @@ function updateCart() {
 }
 
 function sendOrder(name, price) {
-	const data = {
-		toyName: name,
-		price: price,
-	};
+	Toastify({
+		text: `${name} - добавлен`,
+		duration: 2000,
+		destination: "https://github.com/apvarun/toastify-js",
+		newWindow: true,
+		gravity: "top",
+		position: "center",
+		stopOnFocus: true,
+		style: {
+			background: "var(--linear-gradient)",
+			boxShadow: "0px 7px 24px 0 rgba(255, 255, 255, 0.05)",
+			fontSize: "14px",
+			padding: "10px 18px",
+		},
+		onClick: function () {},
+	}).showToast();
 
 	cart.push({ name, price, quantity: 1 });
 	app.HapticFeedback.notificationOccurred("success");
 	updateCart();
-
-	app.sendData(JSON.stringify(data));
 }
 
 app.MainButton.onClick(() => {
@@ -57,5 +132,5 @@ app.MainButton.onClick(() => {
 	};
 
 	app.sendData(JSON.stringify(orderData));
-	app.close();
+	// app.close();
 });
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const { Telegraf, Markup } = require("telegraf");
 
-// const SELLER_ID = process.env.SELLER_ID;
+const SELLER_ID = process.env.SELLER_ID;
 const BOT_TOKEN = process.env.BOT_TOKEN;
 if (!BOT_TOKEN) throw new Error("BOT_TOKEN must be provided!");
 
@@ -11,111 +11,72 @@ const WEB_APP_URL = process.env.WEB_APP_URL;
 
 bot.command("start", (ctx) => {
 	ctx.reply(
-		"Нажмите, чтобы открыть каталог:",
-		Markup.inlineKeyboard([
-			[Markup.button.webApp("Открыть каталог 🧸", WEB_APP_URL)],
-		])
+		"Выберите действие:",
+		Markup.keyboard([
+			Markup.button.webApp("Открыть каталог 🧸", WEB_APP_URL)
+		]).resize()
 	);
 });
 
-// bot.command("start", (ctx) => {
-// 	return ctx.reply(
-// 		"Выберите действие:",
-// 		Markup.inlineKeyboard([
-// 			[Markup.button.callback("Купить игрушку 🧸", "buy_toy")],
-// 		])
-// 	);
-// });
+bot.command("web", (ctx) => {
+	ctx.replyWithMarkdownV2("Наш оригинальный [сайт](https://dev-richer.ru)");
+});
 
 bot.command("id", (ctx) => {
 	ctx.reply(`Ваш Telegram ID: ${ctx.from.id}`);
 });
 
-// bot.action("buy_toy", async (ctx) => {
-// 	await ctx.answerCbQuery();
-
-// 	const user = ctx.from;
-
-// 	const toyName = "Мягкая игрушка Лисёнок";
-// 	const price = "1500 ₽";
-
-// 	// Сообщение продавцу
-// 	await ctx.telegram.sendMessage(
-// 		SELLER_ID,
-// 		`<b>🛒 Новая заявка!</b>
-
-// 👤 Клиент: @${user.username || "нет username"}
-// 🆔 ID: ${user.id}
-
-// 🎁 Товар: <b>Мягкая игрушка Лисёнок</b>
-// 💵 Цена: <b>1500 ₽</b>
-
-// 💬 Приветствие:
-// "Здравствуйте! Клиент интересуется этой игрушкой."`,
+// bot.hears("Написать продавцу 📨", (ctx) => {
+// 	ctx.replyWithPhoto(
+// 		{ url: "./app/assets/img/user/" },
 // 		{
-// 			parse_mode: "HTML",
-// 			reply_markup: Markup.inlineKeyboard([
-// 				[
-// 					Markup.button.url(
-// 						"Написать клиенту",
-// 						`https://t.me/${user.username}`
-// 					),
-// 				],
-// 			]),
+// 			caption: `🧑‍💼 Продавец: Иван Иванов\n📱 Телефон: +7 999 123-45-67\n🌐 Telegram: [@username_pravovogo](https://t.me/username_pravovogo)\n💬 Работаем с 10:00 до 20:00`,
+// 			parse_mode: "Markdown",
 // 		}
 // 	);
-
-// 	// Ответ клиенту
-// 	await ctx.reply(`Вы подключены к продавцу!\nОн скоро с вами свяжется 👌`);
 // });
 
-// bot.on("web_app_data", async (ctx) => {
-// 	try {
-// 		const data = JSON.parse(ctx.message.web_app_data.data);
-// 		const user = ctx.from;
+bot.on("web_app_data", async (ctx) => {
+	try {
+		const data = JSON.parse(ctx.message.web_app_data.data);
+		const user = ctx.from;
 
-// 		// Данные из Mini App
-// 		const toyName = data.toyName;
-// 		const price = data.price;
+		// Данные из Mini App
+		const toyName = data.toyName;
+		const price = data.price;
 
-// 		const contactLink = user.username
-// 			? `https://t.me/${user.username}`
-// 			: `tg://user?id=${user.id}`;
+		const contactLink = user.username
+			? `https://t.me/${user.username}`
+			: `tg://user?id=${user.id}`;
 
-// 		// Отправляем продавцу
-// 		await ctx.telegram.sendMessage(
-// 			SELLER_ID,
-// 			`<b>🛒 Новая заявка!</b>
+		// Отправляем продавцу
+		await ctx.telegram.sendMessage(
+			SELLER_ID,
+			`<b>🛒 Новая заявка!</b>
+👤 Клиент: @${user.username || "нет username"}
+🆔 ID: ${user.id}
 
-// 👤 Клиент: @${user.username || "нет username"}
-// 🆔 ID: ${user.id}
+🎁 Товар: <b>${toyName}</b>
+💵 Цена: <b>${price}</b>
 
-// 🎁 Товар: <b>${toyName}</b>
-// 💵 Цена: <b>${price}</b>
+💬 Приветствие:
+"Здравствуйте! Клиент интересуется этой игрушкой."`,
+			{
+				parse_mode: "HTML",
+				reply_markup: Markup.inlineKeyboard([
+					[Markup.button.url("Написать клиенту", contactLink)],
+				]),
+			}
+		);
 
-// 💬 Приветствие:
-// "Здравствуйте! Клиент интересуется этой игрушкой."`,
-// 			{
-// 				parse_mode: "HTML",
-// 				reply_markup: Markup.inlineKeyboard([
-// 					[Markup.button.url("Написать клиенту", contactLink)],
-// 				]),
-// 			}
-// 		);
-
-// 		// Ответ клиенту
-// 		await ctx.reply(
-// 			"Вы подключены к продавцу! Он скоро с вами свяжется 👌"
-// 		);
-// 	} catch (err) {
-// 		console.error(err);
-// 	}
-// });
-
-// bot.action('web_app_data', async (ctx) => {
-//   console.log('Данные из Mini App:', ctx.message.web_app_data.data);
-//   await ctx.reply('Бот получил данные!');
-// });
+		// Ответ клиенту
+		await ctx.reply(
+			"Вы подключены к продавцу! Он скоро с вами свяжется 👌"
+		);
+	} catch (err) {
+		console.error(err);
+	}
+});
 
 bot.launch();
 
